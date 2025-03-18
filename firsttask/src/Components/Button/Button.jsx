5284db514686b45453b48bd3f08f18bf0246b98d
Sakshi-Kolekar <sakshikolekar1611@gmail.com>
2025-03-18 15:44:03 +0530

    created second day task's project with tailwind setup!

--- firsttask/src/Components/Button/Button.jsx
+++ firsttask/src/Components/Button/Button.jsx
@@ -7,8 +7,8 @@ function Button() {
 
     return (
         <>
-            <button onClick={logMsg} type="button" className="mt-4 w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition">
-                Click me</button>
+            <button onClick={logMsg} type="button" class="w-full text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Click me</button>
         </>
     )
 }
