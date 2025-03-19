436f5ecc8267834828599def9cb3fb5ad250bfcd
Sakshi-Kolekar <sakshikolekar1611@gmail.com>
2025-03-19 10:49:54 +0530

    Dummy api loaded with the additional loader with settimeOut to See Loader is Working or not

--- secondtask/src/App.jsx
+++ secondtask/src/App.jsx
@@ -5,21 +5,24 @@ import Counter from './Component/Counter/Counter'
 import ColorChanger from './Component/ColorChanger/ColorChanger'
 import UseEffectDemo from './Component/UseEffectDemo/UseEffectDemo'
 import InputDemo from './Component/InputDemo/InputDemo'
+import DummyApi from './Component/DummyApi/DummyApi'
 
 function App() {
-  
+
 
   return (
     <>
-     <ToggleButton/>
-     <hr />
-     <Counter/>
-     <hr />
-     <ColorChanger/>
-     <hr />
-     <UseEffectDemo/>
-     <hr />
-     <InputDemo/>
+      <ToggleButton />
+      <hr />
+      <Counter />
+      <hr />
+      <ColorChanger />
+      <hr />
+      <UseEffectDemo />
+      <hr />
+      <InputDemo />
+      <hr />
+      <DummyApi/>
     </>
   )
 }
